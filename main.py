df07202c630a20a26b873df4a3b3504b0ffc4f1d
soul0101 <53980340+soul0101@users.noreply.github.com>
2022-10-17 20:35:24 +0530

    add early daisi loading

--- main.py
+++ main.py
@@ -22,6 +22,10 @@ load_dotenv()
 DIR = os.path.dirname(__file__)
 
 signature_verification_toolkit = pyd.Daisi("soul0101/Signature Verification Toolkit")
+try:
+    signature_verification_toolkit.workers.set(1)
+except:
+    pass
 
 def crop_to_cheque(img, send_steps=False):
     """
@@ -339,19 +343,23 @@ def st_ui():
         cheque_image = st.sidebar.file_uploader("Upload the Cheque Image 📤", type=['png', 'jpg', 'jpeg'])
         cheque_template_name = st.sidebar.selectbox("Choose the cheque template 🧾", cheque_templates_dict.keys())
         cheque_template_path = cheque_templates_dict[cheque_template_name]
+        original_signature_path = './images/original_sign.png' # Default
     else:
         selector_dict = {
             "Sample 1": {
                 "image_path": "./images/icici1_tilted.png",
-                "template_path": ICICI_TEMPLATE_PATH
+                "template_path": ICICI_TEMPLATE_PATH,
+                "signature_path": './images/original_sign.png'
             },
             "Sample 2": {
                 "image_path": "./images/ab1.jpeg",
-                "template_path": AXIS_TEMPLATE_PATH
+                "template_path": AXIS_TEMPLATE_PATH,
+                "signature_path": './images/ab1_sign.png'
             },
             "Sample 3": {
                 "image_path": "./images/sb1.jpeg",
-                "template_path": SYNDICATE_TEMPLATE_PATH
+                "template_path": SYNDICATE_TEMPLATE_PATH,
+                "signature_path": './images/sb1_sign.png'
             }
         } 
 
@@ -359,6 +367,7 @@ def st_ui():
         sample_cheque = selector_dict[document_selector]    
         cheque_image = sample_cheque["image_path"]
         cheque_template_path = sample_cheque["template_path"]
+        original_signature_path = sample_cheque["signature_path"]
         
     if cheque_image is None:
         st.sidebar.markdown("***")
@@ -413,7 +422,7 @@ def st_ui():
             """)
 
         st.markdown("#### Signature Verification")
-        orig_sign_np = cv2.imread(os.path.join(DIR, r'./images/original_sign.png'))
+        orig_sign_np = cv2.imread(original_signature_path)
         check_sign_np = segmentation_result['sign_area']
 
 
